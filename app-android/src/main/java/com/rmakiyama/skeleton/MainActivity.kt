031f4675631c2d2cc60cc794191e396d673f6aca
rmakiyama <rmakiyama.io@gmail.com>
2022-11-23 22:58:09 +0900

    apply edge to edge

--- app-android/src/main/java/com/rmakiyama/skeleton/MainActivity.kt
+++ app-android/src/main/java/com/rmakiyama/skeleton/MainActivity.kt
@@ -10,11 +10,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.rmakiyama.skeleton.theme.SkeletonTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             SkeletonTheme {
                 Surface(
